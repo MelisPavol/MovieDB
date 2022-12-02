c8998ea98a13292fb98064cd080f3d6d85b252e1
Darina Melis <darina05@email.cz>
2022-12-02 22:39:34 +0100

    wip6

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -10,7 +10,7 @@ public class SignInTest extends BaseTest{
     }
     @Test
     public void doTestFailed() {
-        var a = 1;
-        Assert.assertEquals(2, a);
+        var a = 3;
+        Assert.assertEquals(3, a);
     }
 }
